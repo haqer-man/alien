75710a745a9be2bdc3268771cbf0457ac2d1e011
Zachary <zachareee752@gmail.com>
2024-12-09 17:37:13 -0500

    Created v3 and converted files to .ogg

--- alien_v2.py
+++ alien_v2.py
@@ -1,4 +1,4 @@
-from codecs import lookup
+'''from codecs import lookup
 import tkinter as tk
 from PIL import Image, ImageTk
 
@@ -11,6 +11,7 @@ from time import sleep
 from random import randint
 import numpy as np
 from os import listdir
+from os.path import realpath, dirname
 
 # monitor setup
 mon = get_monitors()[0]
@@ -23,7 +24,180 @@ root = tk.Tk()
 root.overrideredirect(True)
 root.wm_attributes('-topmost', True)
 # root.geometry("{}x{}+{}+0".format(WIDTH,HEIGHT,mon1_width))
-root.geometry("{}x{}".format(WIDTH, HEIGHT))
+root.geometry("{}x{}+0+0".format(WIDTH, HEIGHT))
+display = tk.Label(root)
+display.configure(background="black")
+display.pack()
+root.attributes('-alpha', 0.0)
+
+'\''blank = tk.Tk()
+blank.overrideredirect(True)
+# blank.geometry("{}x{}".format(WIDTH, HEIGHT))
+blk = tk.Canvas(blank, width=WIDTH, height=HEIGHT, bg="black")
+blk.pack()''\'
+
+# initialize files and shuffle list
+path = dirname(realpath(__file__)) + "/"
+f = [s for s in listdir(path + "Alien/") if s.endswith(".mp3") and not s.startswith(".")]
+files = np.array(f)
+np.random.shuffle(files)
+
+# generator function to iterate through shuffled list
+def filepath():
+    for f in files:
+        yield path + "Alien/" + f
+
+gif = path + "Alien.gif"
+info = Image.open(gif)
+
+frames = info.n_frames # number of frames in gif
+
+# make list of gif frames
+photoimage_objects = []
+for i in range(frames):
+    obj = tk.PhotoImage(file=gif, format=f"gif -index {i}")
+    photoimage_objects.append(obj.zoom(4))
+
+# initialize pool
+pool = concurrent.futures.ThreadPoolExecutor(max_workers=6)
+main_pool = concurrent.futures.ThreadPoolExecutor(max_workers=1)
+
+
+def animation(current_frame=0):
+    global loop
+
+    print("[i] Running animation.")
+
+    image = photoimage_objects[current_frame]
+
+    display.configure(image=image)
+    current_frame += 1
+
+    if current_frame == frames:
+        current_frame = 0
+
+    loop = root.after(20, lambda: animation(current_frame))
+
+def stop_animation():
+    root.after_cancel(loop)
+    fade_out_gif(root)
+
+def fade_in_gif(window, alpha_value=0.0):
+    if alpha_value < 1.0:
+        window.attributes('-alpha', alpha_value)
+        alpha_value += 0.05
+        window.after(50, fade_in_gif, window, alpha_value)
+
+def fade_out_gif(window, alpha_value=1.0):
+    if alpha_value > 0.0:
+        window.attributes('-alpha', alpha_value)
+        alpha_value -= 0.05
+        window.after(50, fade_out_gif, window, alpha_value)
+
+def fade_in_audio(volume=0):
+    global player
+    if player.audio_get_volume() < 100:
+        player.audio_set_volume(volume)
+        volume += 5
+        sleep(0.2)
+        #pool.submit((fade_in_audio, volume))
+        fade_in_audio(volume)
+
+def fade_out_audio(volume=100):
+    global player
+    if player.audio_get_volume() > 0:
+        player.audio_set_volume(volume)
+        volume -= 5
+        sleep(0.2)
+        fade_out_audio(volume)
+
+def play_video(starting_position: float, duration: float):
+    global player
+
+    print("[i] Made it to play_video function.")
+    if not player.is_playing():
+        print("[i] Player is not currently playing. Continuing.")
+        # player = vlc.MediaPlayer("file://{}".format(fi))
+        player.audio_set_volume(0)
+        player.set_position(starting_position)
+        pool.submit(animation)
+        root.after(20, fade_in_gif, root)
+        sleep(0.1)
+        player.play()
+        pool.submit(fade_in_audio)
+    
+def main():
+    # pool.submit(blank.mainloop)
+    global player
+    # sleep(randint(5, 10))
+
+    # root.after(20, fade_in, root)
+    # play_video()
+    while True:
+        # sleep(randint(300, 3600))
+        # sleep(randint(180, 300))
+        sleep(10)
+        print("[i] Slept for 10.")
+        
+        fi = next(f)
+        player = vlc.MediaPlayer("file://{}".format(fi))
+        print("[i] Initialized MediaPlayer.")
+
+        sleep(2)
+        print("[i] Slept for 2.")
+
+        total_duration = player.get_length()
+        start_time = randint(0, total_duration - 10)
+        start_position = start_time / total_duration
+        played_duration = randint(5, total_duration - start_time)
+
+
+        play_video(start_position, played_duration)
+
+        sleep(1)
+        
+        print("Playing {} (total of {} seconds). starting at {}. playing for {}"\
+        .format(fi, total_duration, start_time, played_duration))
+        sleep(played_duration / 1000 - 2)
+
+        fade_out_gif(root)
+        fade_out_audio()
+        player.stop()
+        root.after(1000, stop_animation)
+                
+f = filepath()
+
+player = vlc.MediaPlayer()
+
+main_pool.submit(main)
+root.mainloop()'''
+
+import tkinter as tk
+from PIL import Image, ImageTk
+
+# audio module imports
+import vlc
+
+from screeninfo import get_monitors
+import concurrent.futures
+from time import sleep
+from random import randint
+import numpy as np
+from os import listdir
+from os.path import realpath, dirname
+
+# monitor setup
+mon = get_monitors()[0]
+RES = (WIDTH, HEIGHT) = (mon.width, mon.height)
+
+mon1_width = get_monitors()[0].width
+
+# Initialize tk
+root = tk.Tk()
+root.overrideredirect(True)
+root.wm_attributes('-topmost', True)
+# root.geometry("{}x{}+{}+0".format(WIDTH,HEIGHT,mon1_width))
+root.geometry("{}x{}+0+0".format(WIDTH, HEIGHT))
 display = tk.Label(root)
 display.configure(background="black")
 display.pack()
@@ -36,17 +210,17 @@ blk = tk.Canvas(blank, width=WIDTH, height=HEIGHT, bg="black")
 blk.pack()'''
 
 # initialize files and shuffle list
-path = '/Users/ZBurgoon/Desktop/Alien/'
-f = [s for s in listdir(path) if s.endswith(".mp3") and not s.startswith(".")]
+path = dirname(realpath(__file__)) + "/"
+f = [s for s in listdir(path + "Alien/") if s.endswith(".mp3") and not s.startswith(".")]
 files = np.array(f)
 np.random.shuffle(files)
 
 # generator function to iterate through shuffled list
 def filepath():
     for f in files:
-        yield path + f
+        yield path + "Alien/" + f
 
-gif = "/Users/ZBurgoon/Downloads/Alien.gif"
+gif = path + "Alien.gif"
 info = Image.open(gif)
 
 frames = info.n_frames # number of frames in gif
@@ -85,7 +259,7 @@ def fade_in_gif(window, alpha_value=0.0):
         window.after(50, fade_in_gif, window, alpha_value)
 
 def fade_out_gif(window, alpha_value=1.0):
-    if alpha_value > 0.0:
+    if window.attributes()[1] > 0.0:
         window.attributes('-alpha', alpha_value)
         alpha_value -= 0.05
         window.after(50, fade_out_gif, window, alpha_value)
@@ -121,8 +295,8 @@ def main():
     # play_video()
     while True:
         # sleep(randint(300, 3600))
-        sleep(randint(180, 300))
-        # sleep(15)
+        # sleep(randint(180, 300))
+        sleep(15)
         
 
         play_video()
---
+++ alien_v3.py
@@ -0,0 +1,149 @@
+from pygame.mixer import *
+import audioread
+import tkinter as tk
+from PIL import Image, ImageTk
+
+from screeninfo import get_monitors
+import concurrent.futures
+import threading
+from time import sleep
+from random import randint, shuffle
+from os import listdir
+from os.path import realpath, dirname
+
+# monitor setup
+mon = get_monitors()[0]
+# RES = (WIDTH, HEIGHT) = (mon.width, mon.height)
+RES = (WIDTH, HEIGHT) = (mon.width, mon.height)
+
+mon1_width = get_monitors()[0].width
+
+# Initialize tk
+root = tk.Tk()
+root.overrideredirect(True)
+root.wm_attributes('-topmost', True)
+root.geometry("{}x{}+0+0".format(WIDTH, HEIGHT))
+display = tk.Label(root)
+display.configure(background="black")
+display.pack()
+root.attributes('-alpha', 0.0)
+
+# Initialzie pygame mixer
+init()
+
+# initialize files and shuffle list
+path = dirname(realpath(__file__)) + "/"
+files = [s for s in listdir(path + "Alien/") if s.endswith(".mp3") and not s.startswith(".")]
+shuffle(files)
+
+# generator function to iterate through shuffled list
+def filepath():
+    for f in files:
+        yield path + "Alien/" + f
+
+gif = path + "Alien.gif"
+info = Image.open(gif)
+
+frames = info.n_frames # number of frames in gif
+
+# make list of gif frames
+photoimage_objects = []
+for i in range(frames):
+    obj = tk.PhotoImage(file=gif, format=f"gif -index {i}")
+    photoimage_objects.append(obj.zoom(4))
+
+# initialize pools
+pool = concurrent.futures.ThreadPoolExecutor(max_workers=6)
+main_pool = concurrent.futures.ThreadPoolExecutor(max_workers=1)
+
+def animation(current_frame=0):
+    global loop
+    image = photoimage_objects[current_frame]
+
+    display.configure(image=image)
+    current_frame += 1
+
+    if current_frame == frames:
+        current_frame = 0
+
+    loop = root.after(20, lambda: animation(current_frame))
+
+def stop_animation():
+    root.after_cancel(loop)
+    fade_out_gif(root)
+
+def fade_in_gif(window, alpha_value=0.0):
+    if alpha_value < 1.0:
+        window.attributes('-alpha', alpha_value)
+        alpha_value += 0.05
+        window.after(50, fade_in_gif, window, alpha_value)
+
+def fade_out_gif(window, alpha_value=1.0):
+    if window.attributes()[1] > 0.0:
+        window.attributes('-alpha', alpha_value)
+        alpha_value -= 0.05
+        window.after(50, fade_out_gif, window, alpha_value)
+
+def fade_in_audio(volume=0.0):
+    if music.get_volume() < 1.0:
+        music.set_volume(volume)
+        volume += 0.03
+        sleep(0.2)
+        fade_in_audio(volume)
+
+def fade_out_audio(volume=1.0):
+    if music.get_volume() > 0.0:
+        music.set_volume(volume)
+        volume -= 0.05
+        sleep(0.2)
+        fade_out_audio(volume)
+
+def calculate_timing(duration):
+    start_time = randint(0, duration - 10)
+    played_duration = randint(5, duration - start_time)
+
+    return start_time, played_duration
+
+def play_random_part_of_video():
+    if not music.get_busy():
+        fi = next(f)
+        print(fi)
+        totalsec = 0
+        with audioread.audio_open(fi) as fl: 
+            totalsec = int(fl.duration)
+
+        start_time, played_duration = calculate_timing(totalsec)
+
+        music.load(fi)
+        music.set_volume(0)
+
+        print("Playing {} (total of {} seconds). Starting at {} and playing for {} seconds"\
+        .format(fi, totalsec, start_time, played_duration))
+        pool.submit(animation)
+        root.after(20, fade_in_gif, root)
+        sleep(0.1)
+        music.play()
+        music.set_pos(start_time)
+
+        pool.submit(fade_in_audio)
+        sleep(played_duration)
+        root.after(20, fade_out_gif, root)
+
+        fade_out_audio()
+        root.after(20, fade_out_gif, root)
+        stop_animation()
+        music.stop()
+        music.unload()
+
+def main():
+    while True:
+        # sleep(randint(180,300))
+        sleep(5)
+
+        play_random_part_of_video()
+
+f = filepath()
+
+pool.submit(main)
+
+root.mainloop()
